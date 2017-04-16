983c5485666d9591eb6f3391e057e606c484b30d
Carl Wistedt <carl.wistedt@capgemini.com>
2017-04-17 00:01:49 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MomentModule } from 'angular2-moment';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
+import { HackernewsApiService } from './hackernews-api.service';
+import { DomainPipe } from './domain.pipe';
+import { ItemCommentsComponent } from './item-comments/item-comments.component';
+import { routing } from './app.routes';
+import { CommentTreeComponent } from './comment-tree/comment-tree.component';
+import { CommentComponent } from './comment/comment.component';
+import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent,
+    DomainPipe,
+    ItemCommentsComponent,
+    CommentTreeComponent,
+    CommentComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MomentModule,
+    routing
   ],
-  providers: [],
+  providers: [HackernewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/hackernews-api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class HackernewsApiService {
+  baseUrl: string;
+
+  constructor(private http:Http) {
+    this.baseUrl = 'https://node-hnapi.herokuapp.com';
+   }
+
+   public fetchStories(storyType: string, page: number): Observable<any> {
+     return this.http.get(`${this.baseUrl}/${storyType}?page=${page}`)
+        .map(response => response.json());
+   }
+
+   public fetchItem(id: number): Observable<any> {
+     return this.http.get(`${this.baseUrl}/item/${id}.json`)
+      .map(response => response.json());
+   }
+
+   public fetchComments(id: number): Observable<any> {
+     return this.http.get(`${this.baseUrl}/item/${id}`)
+                      .map(response => response.json());
+   }
+
+   public fetchUser(id: number): Observable<any> {
+     return this.http.get(`${this.baseUrl}/user/${id}`)
+                      .map(response => response.json());
+   }
+   
+}
